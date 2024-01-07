de258bcc123ab27d7cce6b8a8f923b199c38ecb0
tejeshwarv16 <155345149+tejeshwarv16@users.noreply.github.com>
2024-01-07 15:46:17 +0530

    Update age.py

--- age.py
+++ age.py
@@ -17,23 +17,17 @@ from sklearn.model_selection import train_test_split
 path = "D:/projects/age/dataset/UTKFace"
 images = []
 age = []
-gender = []
 for img in os.listdir(path):
   ages = img.split("_")[0]
-  genders = img.split("_")[1]
   img = cv2.imread(str(path)+"/"+str(img))
   img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
   images.append(np.array(img))
   age.append(np.array(ages))
-  gender.append(np.array(genders))
   
 age = np.array(age,dtype=np.int64)
 images = np.array(images)   #Forgot to scale image for my training. Please divide by 255 to scale. 
-gender = np.array(gender,np.uint64)
 
 x_train_age, x_test_age, y_train_age, y_test_age = train_test_split(images, age, random_state=42)
-
-x_train_gender, x_test_gender, y_train_gender, y_test_gender = train_test_split(images, gender, random_state=42)
 
 ##################################################
 #Define age model and train. 
@@ -70,82 +64,5 @@ history_age = age_model.fit(x_train_age, y_train_age,
 
 age_model.save('age_model_50epochs.h5')
 
-################################################################
-#Define gender model and train
-##################################################
-gender_model = Sequential()
-
-gender_model.add(Conv2D(36, kernel_size=3, activation='relu', input_shape=(200,200,3)))
-
-gender_model.add(MaxPool2D(pool_size=3, strides=2))
-gender_model.add(Conv2D(64, kernel_size=3, activation='relu'))
-gender_model.add(MaxPool2D(pool_size=3, strides=2))
-
-gender_model.add(Conv2D(128, kernel_size=3, activation='relu'))
-gender_model.add(MaxPool2D(pool_size=3, strides=2))
-
-gender_model.add(Conv2D(256, kernel_size=3, activation='relu'))
-gender_model.add(MaxPool2D(pool_size=3, strides=2))
-
-gender_model.add(Conv2D(512, kernel_size=3, activation='relu'))
-gender_model.add(MaxPool2D(pool_size=3, strides=2))
-
-gender_model.add(Flatten())
-gender_model.add(Dropout(0.2))
-gender_model.add(Dense(512, activation='relu'))
-gender_model.add(Dense(1, activation='sigmoid', name='gender'))
-
-gender_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-history_gender = gender_model.fit(x_train_gender, y_train_gender,
-                        validation_data=(x_test_gender, y_test_gender), epochs=50)
-
-gender_model.save('gender_model_50epochs.h5')
 
 
-############################################################
-
-history = history_age
-
-#plot the training and validation accuracy and loss at each epoch
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-epochs = range(1, len(loss) + 1)
-plt.plot(epochs, loss, 'y', label='Training loss')
-plt.plot(epochs, val_loss, 'r', label='Validation loss')
-plt.title('Training and validation loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-plt.show()
-
-acc = history.history['accuracy']
-#acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-#val_acc = history.history['val_accuracy']
-
-plt.plot(epochs, acc, 'y', label='Training acc')
-plt.plot(epochs, val_acc, 'r', label='Validation acc')
-plt.title('Training and validation accuracy')
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.legend()
-plt.show()
-
-####################################################################
-from keras.models import load_model
-#Test the model
-my_model = load_model('gender_model_50epochs.h5', compile=False)
-
-
-predictions = my_model.predict(x_test_gender)
-y_pred = (predictions>= 0.5).astype(int)[:,0]
-
-from sklearn import metrics
-print ("Accuracy = ", metrics.accuracy_score(y_test_gender, y_pred))
-
-#Confusion Matrix - verify accuracy of each class
-from sklearn.metrics import confusion_matrix
-import seaborn as sns
-cm=confusion_matrix(y_test_gender, y_pred)  
-sns.heatmap(cm, annot=True)
